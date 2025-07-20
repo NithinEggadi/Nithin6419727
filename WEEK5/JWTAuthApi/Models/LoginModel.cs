c8420cb5a393f8f8ca2c33cf23e0dd3ce7900921
NithinEggadi <nithineggadi2003@gmail.com>
2025-07-20 22:27:28 +0530

    Add files via upload

---
+++ WEEK5/JWTAuthApi/Models/LoginModel.cs
@@ -0,0 +1,8 @@
+namespace JwtAuthApi.Models
+{
+    public class LoginModel
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
